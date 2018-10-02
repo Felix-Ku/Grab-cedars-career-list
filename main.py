637ec20b5b2d2107748aa118e7372051a85c9f93
Felix Ku <43415565+Felix-Ku@users.noreply.github.com>
2018-10-02 16:31:55 +0800

    Update main.py
    To-do:
    - Identify whether the files are good to download like PDF or not
    - Automatically login HKU portal for intraweb download

--- main.py
+++ main.py
@@ -20,50 +20,51 @@ dates=[]
 names=[]
 links=[]
 
-check=True
+check=False
 
 for index in soup.findAll('tr'):
-    for date in index.findAll('i'):
-        dates.append(date.get_text())
-        check=False
     for link in index.findAll('a', {'style': 'font-family: Verdana; font-size: 12px; letter-spacing: normal;'}):
         names.append(link.get_text())
         links.append(link.get('href'))
         dates.append(" ")
         check=True
+    for date in index.findAll('i'):
+        dates.append(date.get_text())
+        check=False
     if check==True:
         dates.pop()
         
 # Loop to assign every record a date  
-for i in (range(dates)-1):
+for i in range((len(dates)-1)):
     if dates[i+1]==" ":
         dates[i+1]==dates[i]
         
 
 # Main selection process
-for i in range(dates):
-    next=raw_input('Show the next & following record(s)? [Y/N]')
-    if next=='N':
+for i in range((len(dates)-1)):
+    next=input('Show the next & following record(s)? [Y/N] ')
+    if next=='N' or next=='n':
         break
     else:
-        print ('Record.',i)
+        print ('Record.',i+1)
         print ('Posted date: ',dates[i])
         print ('Name of career/event: ',names[i])
-        print ('Interested?')
-        choice=raw_input('[Y/N]')
-        if choice==Y:
+        print ('Interested? ')
+        choice=input('[Y/N]')
+        if choice=='Y' or choice=='y':
            # Save the interested data to files
-           urllib.request.urlretrieve(links[i], 'D:\saved_career_data\'+dates[i]+'\'+links[i].split('/')[-1])
+           localpath="D:\saved_career_data\%s"%(links[i].split('/')[-1])
+           urllib.request.urlretrieve(links[i],localpath)
            print('File has been saved successfully!')                           
 
 print ('This is the end of records')
 
 # Create CSV file for all records
-create_csv=raw_input('Create a CSV file to store all records? [Y/N]')
-if create_csv=='Y':                                    
+create_csv=input('Create a CSV file to store all records? [Y/N] ')
+if create_csv=='Y' or create_csv=='y':                                    
     raw={'Names':names,'Links':links}
     df=pd.DataFrame(raw,columns=['Names','Links'],index=dates)
-    df.to_csv('d:\saved_career_data\'+dates[i]+'_updated.csv',encoding='utf_8_sig')
+    df.to_csv('d:\saved_career_data\%s_updated.csv'%(dates[i]),encoding='utf_8_sig')
     print ('CSV file generated successfully')
 
 print ('Good luck!')
